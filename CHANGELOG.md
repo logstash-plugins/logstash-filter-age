56a5b6271e8e8a975e4dd05a802c296593c78900
Richard Mauri <richard.mauri@elliemae.com>
2020-10-09 14:50:29 -0700

    tidy

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.1.0
+  - Add support for a limit service and perform the age criteria test internally
+
 ## 1.0.2
   - Fix some documentation issues
 
--- lib/logstash/filters/age.rb
+++ lib/logstash/filters/age.rb
@@ -16,16 +16,55 @@ class LogStash::Filters::Age < LogStash::Filters::Base
   # Define the target field for the event age, in seconds.
   config :target, :default => '[@metadata][age]', :validate => :string
 
-  # Define the elasticsearch url to the limit service
-  config :url, :default => 'https://elasticsearch.main.top.elliemae.io/_cluster/settings?filter_path=persistent.cluster.metadata.logstash.filter.age.limit_secs', :validate => :string
-
-  config :limit_path, :default => 'persistent.cluster.metadata.logstash.filter.age.limit_secs', :validate => :string
-
-  # The max_age_secs is the default number of seconds beyond which the expired_target will be set to true (when the limit service url not found or no result))
+  # There are two optional features that can be enabled:
+  #
+  # 1. Have age perform the calculation:
+  #   if current time - event time > max_age_secs set the boolean value in
+  #   the expired_target field. 
+  #   This feature is enabled by setting:
+  #   a) a non zero value in the max_age_secs setting.
+  #
+  # 2. Have age determine age_limit to be used in the calculation:
+  #   if current time - event time > age_limit max_age_secs
+  #     set the boolean value in the expired_target field. 
+  #   This feature is enabled by setting:
+  #   a) max_age_secs must be non zero
+  #   b) url must be defined (the url to a limit service returning a json to a 
+  #      leaf level age limit value)
+  #   c) user and password to the url service
+  #      TODO: support other auth approaces
+  #   d) limit_path defined as the lmit service json response body path to
+  #      the age_limit value
+  #   e) interval is the frequency between url requests to get the latest
+  #      age limit.
+  #   f) age_limit_target is the field to store the discovered age limit
+  #
+  # The max_age_secs is the default number of seconds beyond which the 
+  # expired_target will be set to true (when the limit service url not found or 
+  # there is no result)
   config :max_age_secs, :default => 259200, :validate => :number
 
-  # The expired_target field is boolean when the event timestamp  is older than age_limit
-  config :expired_target, :default => '[@metadata][expired]', :validate => :string
+  # Define the elasticsearch url to the limit service
+  config :url, :default => '', :validate => :string
+
+  # The response to the limit url will be a json with a nested structure the ends
+  # in a numeric age limit . The limit_path is a dot delimited representation
+  # of the nested json returned in the limit service response body.
+  config :limit_path,
+    :default => 'persistent.cluster.metadata.logstash.filter.age.limit_secs', 
+    :validate => :string
+
+  # The expired_target field is true when the event is older than age_limit
+  config :expired_target, 
+    :default => '[@metadata][expired]',
+    :validate => :string
+
+  # The age_limit_target is the name of the field whose value is the number of 
+  # seconds actually used in the calculated result stored in expired_target
+  # When url is defined and the limit is found, then this is the discovered value
+  config :age_limit_target, 
+    :default => '[@metadata][age_limit]',
+    :validate => :string
 
   # The interval between calls to the limit service given by the url
   config :interval, :default => "60s", :validate => :string
@@ -35,42 +74,22 @@ class LogStash::Filters::Age < LogStash::Filters::Base
 
   public
   def register
-    @split_limit_path = limit_path.split(".")
-    @scheduler = Rufus::Scheduler.new
+    if url != ''
+      @logger.debug('age filter is configured to use a limit service')
+      @split_limit_path = limit_path.split(".")
+      @scheduler = Rufus::Scheduler.new
 
-    request_limit()
-
-    @scheduler.every @interval do
       request_limit()
+
+      @scheduler.every @interval do
+        request_limit()
+      end
+    else
+      @logger.debug('age filter is not configured to use a limit service')
+      @age_limit = @max_age_secs.to_f
     end
   end
 
-# See: https://www.elastic.co/guide/en/logstash/current/filter-new-plugin.html
-# See: https://github.com/logstash-plugins/logstash-filter-elasticsearch/blob/master/lib/logstash/filters/elasticsearch.rb
-# See: https://github.com/logstash-plugins/logstash-filter-http/blob/master/lib/logstash/filters/http.rb
-# See: https://www.rubydoc.info/gems/logstash-mixin-http_client/4.0.2/LogStash/PluginMixins/HttpClient
-# See: https://github.com/logstash-plugins/logstash-input-http_poller
-# See: https://www.elastic.co/guide/en/logstash/current/event-api.html
-# See: https://discuss.elastic.co/t/how-to-parse-json-values-from-http-poller-into-event-fields/136597
-# See: https://www.elastic.co/guide/en/logstash/current/multiple-input-output-plugins.html
-#
-# curl -u 'obs_ro:Password!234' https://elasticsearch.main.top.elliemae.io/_cluster/settings?filter_path=**.metadata
-# curl -u 'obs_ro:Password!234' https://elasticsearch.main.dev.top.rd.elliemae.io/_cluster/settings?filter_path=persistent.cluster.metadata.logstash.filter.age.limit_secs
-#{
-#  "persistent" : {
-#    "cluster" : {
-#      "metadata" : {
-#        "logstash" : {
-#          "filter" : {
-#            "age" : {
-#              "limit_secs" : "10"
-#            }
-#          }
-#        }
-#      }
-#    }
-#  }
-#}
   public
   def filter(event)
 
@@ -78,12 +97,12 @@ class LogStash::Filters::Age < LogStash::Filters::Base
     event.set(@target, delta)
 
     if delta > @age_limit
-    	event.set(@expired_target, true)
+      event.set(@expired_target, true)
     else
-    	event.set(@expired_target, false)
+      event.set(@expired_target, false)
     end
 
-    event.set("age_limit", @age_limit)
+    event.set(@age_limit_target, @age_limit)
 
     # filter_matched should go in the last line of our successful code
     filter_matched(event)
@@ -93,8 +112,9 @@ class LogStash::Filters::Age < LogStash::Filters::Base
   def request_limit
     begin
 
-      # options = {auth: {user: @user, password: @password.value}}
-      options = {auth: {user: @user, password: @password.value}, request_timeout: @request_timeout, socket_timeout: @socket_timeout, connect_timeout: @connect_timeout, automatic_retries: @automatic_retries}
+      options = {auth: {user: @user, password: @password.value},
+        request_timeout: @request_timeout, socket_timeout: @socket_timeout,
+        connect_timeout: @connect_timeout, automatic_retries: @automatic_retries}
 
       code, response_headers, response_body = request_http(@url, options)
 
@@ -104,13 +124,14 @@ class LogStash::Filters::Age < LogStash::Filters::Base
 
     if client_error
       @logger.error('error during HTTP request',
-                    :url => @url,
-                    :client_error => client_error.message)
+        :url => @url,
+        :client_error => client_error.message)
 
     elsif !code.between?(200, 299)
       @logger.error('error during HTTP request',
-                    :url => @url, :code => code,
-                    :response => response_body)
+        :url => @url,
+        :code => code,
+        :response => response_body)
     else
       process_response(response_body)
     end
@@ -127,20 +148,20 @@ class LogStash::Filters::Age < LogStash::Filters::Base
       parsed = LogStash::Json.load(body).to_hash
 
       @split_limit_path.each do |field|
-          break if !parsed
-          parsed = parsed.dig(field)
+        break if !parsed
+        parsed = parsed.dig(field)
       end
 
       if parsed
-          if parsed.is_a? Numeric
-              @age_limit = parsed.to_f
-              @logger.info('age response parsed numeric',
-                :age_limit => @age_limit, :parsed => parsed)
-          else
-              @age_limit = @max_age_secs.to_f
-              @logger.info('age response parsed non numeric',
-                :age_limit => @age_limit, :parsed => parsed)
-          end
+        if parsed.is_a? Numeric
+          @age_limit = parsed.to_f
+          @logger.info('age response parsed numeric',
+            :age_limit => @age_limit, :parsed => parsed)
+        else
+          @age_limit = @max_age_secs.to_f
+          @logger.info('age response parsed non numeric',
+            :age_limit => @age_limit, :parsed => parsed)
+        end
       else
           @age_limit = @max_age_secs.to_f
           @logger.info('age response parsed false (using max_age_secs)',
@@ -148,7 +169,7 @@ class LogStash::Filters::Age < LogStash::Filters::Base
       end
 
     rescue => e
-        @logger.warn('JSON parsing error', :message => e.message, :body => body)
+      @logger.warn('JSON parsing error', :message => e.message, :body => body)
     end
   end
 end
